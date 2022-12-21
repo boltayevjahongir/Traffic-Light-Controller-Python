3339fb2d876f19b7442159af53f12febd15e90c1
boltayev.jahongir <boltayev.jahongir@gmail.com>
2022-12-21 08:16:10 +0500

    add 4

--- Trafic_control_01.py
+++ Trafic_control_01.py
@@ -5,19 +5,26 @@ import matplotlib
 matplotlib.use("Tkagg")
 tsikl = 10
 
+
+A = [5, 12]
+
+
 def cars_count_random(min, max):
     cars_count = []
     for item in range(1, tsikl+1):
         cars_count.append(random.randint(min, max))
     return cars_count
 
-def passed_count_random(min, max):
+def passed_count_random(min, max, type):
     passed_count = []
-    for item in range(1, tsikl+1):
-        passed_count.append(random.randint(min, max))
+    for item in range(type, tsikl+type):
+        if item%2==0:
+            passed_count.append(0)
+        else:
+            passed_count.append(random.randint(min, max))
     return passed_count
 
-def unpassed_count(cars_count, passed_count):
+def unpassed_count_AB(cars_count, passed_count):
     unpassed = []
     traffic_light = 1  # 1-Green(yashil), 0-Red(qizil)
     delta=0
@@ -34,23 +41,66 @@ def unpassed_count(cars_count, passed_count):
         unpassed.append(delta)
     print("\n")
     return unpassed
+def unpassed_count(cars_count, passed_count, type):
+    unpassed = []
+    traffic_light = type  # 1-Green(yashil), 0-Red(qizil)
+    delta=0
+    for cars_count_item, passed_count_item in zip(cars_count, passed_count):
+        print(traffic_light, end=",  ")
+        if traffic_light == 1:
+            delta = cars_count_item - passed_count_item + delta
+            if delta < 0:
+                delta = 0
+            traffic_light = 0
+        else:
+            delta = cars_count_item + delta
+            traffic_light = 1
+        unpassed.append(delta)
+    print("\n")
+    return unpassed
 
-A=cars_count_random(9, 16)
-A_passed=passed_count_random(5, 11)
-delta = unpassed_count(A, A_passed)
+
+
+A=cars_count_random(5, 15)
+A_passed=passed_count_random(10, 18, 1)
+B=cars_count_random(5, 15)
+B_passed=passed_count_random(10, 18, 1)
+delta = unpassed_count(A, A_passed, 1)
+deltaB = unpassed_count(B, B_passed, 1)
+
+# CD tomon
+C=cars_count_random(5, 15)
+C_passed=passed_count_random(10, 18, 0)
+D=cars_count_random(5, 15)
+D_passed=passed_count_random(10, 18, 0)
+deltaC = unpassed_count(C, C_passed, 0)
+deltaD = unpassed_count(D, D_passed, 0)
 
 print(A)
 print(A_passed)
 print(delta)
+print("")
+print(C_passed)
+
 
 # ax=plt.axes()
 # ax.set_facecolor('grey')
+plt.figure(1)
 font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
 plt.step(list(range(1, len(A)+1)), A, where='post', label="Mashinalar soni")
 plt.step(list(range(1, len(A_passed)+1)), A_passed, where='post', label="O'tkazish qobiliyati")
 plt.step(list(range(1, len(delta)+1)), delta, where='post', label="Qolib ketganlar")
 plt.legend()
-plt.title("Trafic Light Control", fontdict=font1)
+plt.title("Trafic Light Control (A-B)", fontdict=font1)
+plt.ylim(ymin=0)
+plt.grid(color='green', linestyle='--', linewidth=0.5)
+plt.figure(2)
+font1 = {'family': 'serif', 'color': 'blue', 'size': 20}
+plt.step(list(range(1, len(C)+1)), C, where='post', label="Mashinalar soni")
+plt.step(list(range(1, len(C_passed)+1)), C_passed, where='post', label="O'tkazish qobiliyati")
+plt.step(list(range(1, len(deltaC)+1)), deltaC, where='post', label="Qolib ketganlar")
+plt.legend()
+plt.title("Trafic Light Control(C-D)", fontdict=font1)
 plt.ylim(ymin=0)
 plt.grid(color='green', linestyle='--', linewidth=0.5)
 plt.show()
---
+++ test.py
@@ -0,0 +1,4 @@
+import random
+d=[3, 5]
+a=random.randint(d[0], d[1])
+print(a)
